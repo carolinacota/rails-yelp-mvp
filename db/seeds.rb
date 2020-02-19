51cb1ef697340af6e7713b30a9528292c4e35c7d
Carolina Cota <carolinareycota@gmail.com>
2020-02-19 16:09:33 +0100

    Challenge done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+rest1 = Restaurant.create(
+  name: 'Picanharia dos Amigos',
+  address: 'São Paulo',
+  category: 'belgian'
+)
+
+rest2 = Restaurant.create(
+  name: 'Takê',
+  address: 'Salvador',
+  category: 'japanese'
+)
+
+rest3 = Restaurant.create(
+  name: 'Rascal',
+  address: 'São Paulo',
+  category: 'italian'
+)
+
+rest4 = Restaurant.create(
+  name: 'Soggiorno',
+  address: 'São Paulo',
+  category: 'italian'
+)
+
+rest5 = Restaurant.create(
+  name: 'Hakka',
+  address: 'São Paulo',
+  category: 'japanese'
+)
+
+puts "#{Restaurant.count} has been created."
